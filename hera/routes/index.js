46a91f87342ccf9c7f618a97a85acbbf12679b0b
floss-order <flossOrder@gmail.com>
2021-03-27 02:45:49 +0000

    Implemented config updating after form submission

--- hera/routes/index.js
+++ hera/routes/index.js
@@ -1,8 +1,21 @@
 const express = require('express')
 const router = express.Router()
+const swiperproxy = require('../parser/swiperproxy')
+const path = require('path')
 
 router.get('/', (req, res) => {
     res.render('index', { swiperproxy })
+})
+
+router.post('/swiperproxy', (req, res) => {
+    swiperproxy.sections().forEach(section => {
+        for (let option in swiperproxy._sections[section]) {
+            swiperproxy.set(section, option, req.body[option])
+        }
+    })
+    swiperproxy.write(path.join(__dirname, '../proxy.conf'))
+
+    res.redirect('/')
 })
 
 module.exports = router
